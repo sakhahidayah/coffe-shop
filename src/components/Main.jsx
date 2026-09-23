830ee90499b559cba01fbd14dd884739d33df226
sakhahidayah <sakhahidayahramadhan@gmail.com>
2026-09-23 12:44:07 +0700

    add: md version section Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,29 +5,31 @@ export default function Main() {
   return (
     <>
       <main className="min-h-screen bg-coffee-100 flex mb-20 pb-24">
-        <section className="mt-20 mx-5 flex flex-col gap-4">
-          <div className="flex flex-col gap-1.5">
-            <span className="text-3xl font-stretch-50% text-coffee-800">Perfect Coffe,</span>
-            <span className="text-3xl font-stretch-50% text-coffee-accent">Perfect Moments</span>
+        <section className="mt-20 mx-5 flex flex-col gap-4 ">
+          <div className="flex flex-col gap-3 md:w-3/4">
+            <div className="flex flex-col gap-1.5">
+              <span className="text-3xl font-stretch-50% text-coffee-800 md:text-5xl">Perfect Coffe,</span>
+              <span className="text-3xl font-stretch-50% text-coffee-accent md:text-5xl">Perfect Moments</span>
+            </div>
+            <div className="">
+              <p className="text-primary">Experience the finest coffee beans, expertly roasted and brewed to perfection. Every cup tells a story of passion, quality, and craftsmanship.</p>
+            </div>
           </div>
-          <div>
-            <p className="text-primary">Experience the finest coffee beans, expertly roasted and brewed to perfection. Every cup tells a story of passion, quality, and craftsmanship.</p>
-          </div>
-          <div className="flex flex-col gap-3 w-full mt-2">
-            <button className="bg-button hover:bg-button-hover py-1 rounded flex flex-row items-center justify-center gap-2 text-white text-xs font-semibold">
+          <div className="flex flex-col gap-3 w-full mt-2 md:flex-row md:gap-4 md:items-center md:justify-start">
+            <button className="bg-button hover:bg-button-hover py-1 rounded flex flex-row items-center justify-center transition duration-150 gap-2 text-white text-xs md:text-sm font-semibold md:px-5">
               Explore Menu <MoveRight />
             </button>
-            <button className="bg-white hover:bg-button py-2 rounded  gap-2 text-xs font-semibold border-button text-button hover:text-white">Find Us</button>
+            <button className="bg-white hover:bg-button py-1.25 rounded gap-2 text-xs font-semibold border-button border text-button hover:text-white md:px-6 md:text-sm transition duration-150">Find Us</button>
           </div>
-          <div className="flex flex-row gap-2 w-full mt-10 px-1  justify-between">
-            <p className="flex flex-col gap-1 items-center font-medium text-xl text-coffee-600">
-              10+ <span className="font-normal text-xs text-primary">Years Experience</span>
+          <div className="flex flex-row gap-2 w-full mt-10 px-1  justify-between md:justify-start md:gap-6">
+            <p className="flex flex-col gap-1 items-center font-medium text-xl md:text-2xl text-coffee-600">
+              10+ <span className="font-normal text-xs md:text-sm text-primary">Years Experience</span>
             </p>
-            <p className="flex flex-col gap-1 items-center font-medium text-xl text-coffee-600">
-              50+ <span className="font-normal text-xs text-primary">Coffee Varieties</span>
+            <p className="flex flex-col gap-1 items-center font-medium text-xl md:text-2xl  text-coffee-600">
+              50+ <span className="font-normal text-xs text-primary md:text-sm">Coffee Varieties</span>
             </p>
-            <p className="flex flex-col gap-1 items-center font-medium text-xl text-coffee-600">
-              1000+<span className="font-normal text-xs text-primary">Happy Customers</span>
+            <p className="flex flex-col gap-1 items-center font-medium text-xl md:text-2xl  text-coffee-600">
+              1000+<span className="font-normal text-xs text-primary md:text-sm">Happy Customers</span>
             </p>
           </div>
           <div className="mt-10 flex flex-col relative">
