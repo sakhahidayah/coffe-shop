4935a9e9296367f4aa53190eb48aa235ab8cd4c9
sakhahidayah <sakhahidayahramadhan@gmail.com>
2026-09-23 12:45:31 +0700

    style: transition for any buttons in this page

--- src/sections/MenubarDesktop.jsx
+++ src/sections/MenubarDesktop.jsx
@@ -3,14 +3,14 @@ export default function MenubarDesktop() {
     <>
       <div className="hidden md:block">
         <div className="flex flex-row gap-5 items-center">
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Home</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">About</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Menu</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Contact</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Home</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">About</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Menu</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Contact</h1>
         </div>
       </div>
       <div className="hidden md:block px-3 py-1 bg-button hover:bg-button-hover  rounded ">
-        <button className="text-surface font-medium text-base">Order Now</button>
+        <button className="text-surface font-medium text-base transition duration-150">Order Now</button>
       </div>
     </>
   );
--- src/sections/MenubarMobile.jsx
+++ src/sections/MenubarMobile.jsx
@@ -3,13 +3,13 @@ export default function MenuBarMobile() {
     <>
       <div className="flex flex-col  gap-4 px-7 py-4 border-b-inline  border-b-2 bg-coffee-50">
         <div className="flex flex-col item-start gap-4 mx-3">
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Home</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">About</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Menu</h1>
-          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer">Contact</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Home</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">About</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Menu</h1>
+          <h1 className="text-navbar hover:text-accent font-semibold cursor-pointer transition duration-150">Contact</h1>
         </div>
         <div className="mx-3 w-full  flex justify-center items-center rounded bg-button py-2 hover:bg-button-hover">
-          <button className="text-surface font-medium text-sm">Order Now</button>
+          <button className="text-surface font-medium text-sm transition duration-150">Order Now</button>
         </div>
       </div>
     </>
